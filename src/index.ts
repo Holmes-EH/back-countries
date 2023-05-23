207b6b77b8818276942a44076bd1ed24e30fcef1
HolmesEH <s.holmes.eh@gmail.com>
2023-05-23 10:08:26 +0200

    📦️ install deps + config + init datasource

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata'
+import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
+
+import { AppDataSource } from './data-source'
+import * as path from 'path'
+
+const main = async () => {
+	await AppDataSource.initialize()
+
+	const schema = await buildSchema({
+		resolvers: [path.join(__dirname, './resolvers/*.*')],
+	})
+
+	const apolloServer = new ApolloServer({ schema })
+	try {
+		const { url }: { url: string } = await apolloServer.listen({
+			port: 5000,
+		})
+		console.log(`🚀  Server ready at ${url}`)
+	} catch (error) {
+		console.error('Error starting the server')
+	}
+}
+
+main()
